06ab13a9012b7eb805b1d11cc32550a52d92782c
Naisinn <62951486+Naisinn@users.noreply.github.com>
2024-08-29 19:27:48 +0900

    riverpodスクリプト例

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,43 @@ class HomePage extends ConsumerWidget { // ❷
   }
 }
 
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(counterNotifierProvider);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(counterNotifierProvider.notifier).increment();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
 class CounterNotifier extends Notifier<int> {
   @override
   int build() => 0;
